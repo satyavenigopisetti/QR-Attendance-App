3faa9c5871df7e4b4783edeb1dd6da17384b203e
satyavenigopisetti <satyaveni.gopisetti1234@gmail.com>
2025-06-10 22:05:18 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,1 +1,42 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS")
+    )
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role TEXT NOT NULL
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS attendance (
+        id SERIAL PRIMARY KEY,
+        user_email TEXT,
+        qr_data TEXT,
+        latitude DOUBLE PRECISION,
+        longitude DOUBLE PRECISION,
+        selfie TEXT,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """)
+
+    conn.commit()
+    conn.close()
 
