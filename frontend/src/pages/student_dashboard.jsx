72c578b08b7405557443290d0adc1e610f1c6111
satyavenigopisetti <satyaveni.gopisetti1234@gmail.com>
2025-06-10 22:22:31 +0530

    Update student_dashboard.jsx

--- frontend/src/pages/student_dashboard.jsx
+++ frontend/src/pages/student_dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const StudentDashboard = () => (
-  <div>
-    <h2>Student Dashboard</h2>
-    <a href="/student/scan-qr">Scan QR Code</a>
-  </div>
-);
+function StudentDashboard() {
+  return (
+    <div>
+      <h2>Student Dashboard</h2>
+      <Link to="/student/scan-qr">Scan QR</Link>
+    </div>
+  );
+}
 
 export default StudentDashboard;
