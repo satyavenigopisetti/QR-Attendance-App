4160a677f31be5688f852d3dd3b1cb1cf6fd9c22
satyavenigopisetti <satyaveni.gopisetti1234@gmail.com>
2025-06-10 19:54:03 +0530

    Update QRscanner.js

--- frontend/src/pages/QRscanner.js
+++ frontend/src/pages/QRscanner.js
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
-import QRCode from 'qrcode.react';
+import React, { useEffect } from 'react';
+import QrScanner from 'qr-scanner';
 
-const QRGenerator = () => {
-  const [data, setData] = useState('');
-  return (
-    <div>
-      <h2>Generate QR</h2>
-      <input type="text" placeholder="Enter session ID or data" onChange={(e) => setData(e.target.value)} />
-      <QRCode value={data} />
-    </div>
-  );
+const QRScanner = () => {
+  useEffect(() => {
+    const video = document.getElementById('scanner');
+    QrScanner.WORKER_PATH = '/qr-scanner-worker.min.js';
+    const scanner = new QrScanner(video, result => alert('Scanned: ' + result));
+    scanner.start();
+    return () => scanner.stop();
+  }, []);
+
+  return <video id="scanner" width="300" height="300"></video>;
 };
 
-export default QRGenerator;
+export default QRScanner;
