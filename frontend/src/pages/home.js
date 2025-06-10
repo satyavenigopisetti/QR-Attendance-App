4d329deb2591ce4d8d6693c38ef9ab6a65906c38
satyavenigopisetti <satyaveni.gopisetti1234@gmail.com>
2025-06-10 22:20:24 +0530

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Home = () => (
-  <div className="container">
-    <h1>QR Attendance System</h1>
-    <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
-  </div>
-);
-
+function Home() {
+  return <div><h1>Welcome to QR Attendance System</h1></div>;
+}
 export default Home;
+
