32a5903dfa8b8f31e2d0e6a667d6a4c19a49a38b
satyavenigopisetti <satyaveni.gopisetti1234@gmail.com>
2025-06-10 22:05:42 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
+
+class UserRegister(BaseModel):
+    email: str
+    password: str
+    role: str
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+class QRRequest(BaseModel):
+    session_id: str
+
+class AttendanceData(BaseModel):
+    qr_code_data: str
+    location: dict
+    selfie: str
 
